d51d7c87d4d57fa8c47511a2b0ac818eb7924d27
kairodem <abdallah_3addoula@hotmail.com>
2017-07-12 12:12:27 +0100

    added project files into the repository

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tunilance | Freelance in Tunisia</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+	<script type="text/javascript" src="assets/scripts/app.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700" rel="stylesheet"> 
+</head>
+<body>
+	<div class="header">
+		<div class="wrapper">
+			<div class="logo"><a href=""></a></div>
+			<div class="headnav">
+				<ul>
+					<li><a href="">Log in</a></li>
+					<li><a href="">Sign up</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="undhead">
+		<div class="wrapper">
+			<center>
+				<h1>Become a freelancer today!</h1>
+				<h2>Free *, simple and fast. The best freelance experience for Tunisian creative minds.</h2>
+				<div class="btn"><a href="#" onclick="play()">Start now</a></div><br>
+				<h2>Or</h2><br>
+				<div class="search">
+					<center>
+						<ul>
+							<li><input type="text" name="" placeholder="Search for freelancers, offers, requests..."></li>
+							<li id="sep"></li>
+							<li><a href="">Browse</a></li>
+						</ul>
+					</center>
+				</div>
+				<audio id="aud">
+				  <source src="assets/sound/click.wav" type="audio/wav">
+				  Your browser does not support the audio element.
+				</audio>
+				<script>
+					var x = document.getElementById("aud"); 
+					x.volume = 0.5;
+					function play() { 
+					    x.play(); 
+					}
+				</script>
+				<h3>* Memberships are aquired by users, all users get the same features. <a href="">Learn more</a>.</h3>
+			</center>	
+		</div>
+	</div>
+	<div class="content1">
+		<div class="wrapper">
+			<center>
+				<img src="assets/img/q.svg" width="50px">
+				<h1>What is freelance?</h1>
+				<img src="assets/img/q.svg" width="50px" style="image-orientation:180deg">
+			</center>
+			<p>1.Freelancer. a person who works as a writer, designer, performer, or the like, selling work or services by the hour, day, job, etc., rather than working on a regular salary basis for one employer.<br>2.A person who contends in a cause or in a succession of various causes, as he or she chooses, without personal attachment or allegiance.
+			</p>
+			<center>
+				<img src="assets/img/q.svg" width="50px">
+				<h1>What is Tunilance?</h1>
+				<img src="assets/img/q.svg" width="50px" style="image-orientation:180deg">
+			</center>
+			<p>
+				Tunilance is a platform created by Tunisians for Tunisians. It's for those who want to expand their earnings or start making money using their skills. <br>
+				We know that it's hard for Tunisians to do freelance on other websites due to their hard conditions, rules and user exploitation. <br>
+				That's why Tunilance is the best place to be as we don't require the user to pay to start working. 
+			</p>
+		</div>
+	</div>
+	<div class="content2">
+		<div class="wrapper">
+		<center>
+			<h1>How it works</h1>
+			<div class="steps">
+				<ul id="steps1">
+					<li>
+						<div class="box">
+							<img src="assets/img/create.svg" width="100">
+							<h2>1. Create an account</h2>
+							<h3>Create a free account and get 50 coins for free as a startup.</h3>
+						</div>
+					</li>
+					<li>
+						<div class="box" id="boxsep1">
+							<img src="assets/img/skills.svg" width="100">
+							<h2>2. Select your skills</h2>
+							<h3>Select skills based on what you do best to help us get the best suiting job for you.</h3>
+						</div>
+					</li>
+					<li>
+						<div class="box" id="boxsep2">
+							<img src="assets/img/search.svg" width="100">
+							<h2>3. Look for a job</h2>
+							<h3>Browse through hundreds of jobs, results are filtered according to your skills.</h3>
+						</div>
+					</li>
+					<li>
+						<div class="box">
+							<img src="assets/img/coins.svg" width="100">
+							<h2>4. Make money</h2>
+							<h3>Finish your first Job successfully and get paid right away.</h3>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<h2><a href="">Learn more</a> about paiment methods and the coin system.</h2>
+		</center>
+		</div>
+	</div>
+</body>
+</html>
